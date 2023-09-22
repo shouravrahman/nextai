05e83275308550e5ef551b404666fb0f07f697a6
shouravrahman <shouravrahman006@gmail.com>
2023-09-23 02:40:45 +0600

    feat: basic auth display

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,3 +1,23 @@
-export default function Page() {
-	return <h1>Hello, Next.js!</h1>;
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./lib/auth";
+import { signOut } from "next-auth/react";
+import { SignInButton, SignOutButton } from "../../components/Button";
+import Image from "next/image";
+
+export default async function Page() {
+	const session = await getServerSession(authOptions);
+	return (
+		<main className='flex min-h-[100vh] items-center flex-col justify-center '>
+			<h1 className='bg-white text-black'>{session?.user.name}</h1>
+			<Image
+				className='rounded-full'
+				src={session?.user.image}
+				width={100}
+				height={100}
+				alt='user'
+			/>
+			<h1 className='bg-white text-black'>{session?.user.email}</h1>
+			<div>{session ? <SignOutButton /> : <SignInButton />}</div>
+		</main>
+	);
 }
