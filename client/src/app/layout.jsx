1f5fe9ac5742ceededc2e4142418ad184b53b85c
shouravrahman <shouravrahman006@gmail.com>
2023-09-23 02:40:28 +0600

    feat: added nextauth provider

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,10 +1,14 @@
+import "../styles/globals.css";
+import NextAuthProvider from "./lib/NextAuthProvider";
 export default function Layout({ children }) {
 	return (
 		<html lang='en'>
 			<head>
 				<title>Next.js</title>
 			</head>
-			<body>{children}</body>
+			<body>
+				<NextAuthProvider>{children}</NextAuthProvider>
+			</body>
 		</html>
 	);
 }
