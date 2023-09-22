9c83569fa358a5fd72936cbc84675f4ffd9e0309
shouravrahman <shouravrahman006@gmail.com>
2023-09-23 02:39:19 +0600

    chore: added origins for auth images

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
 	reactStrictMode: true,
 	experimental: { appDir: true },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.discordapp.com",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
