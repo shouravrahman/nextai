6a33ca8368163f9430f343f12b680e01dc40e02e
shouravrahman <shouravrahman006@gmail.com>
2023-09-30 23:46:01 +0600

    feat: button component initial

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { signIn, signOut } from "next-auth/react";
 export const SignInButton = () => {
 	return (
 		<button
-			className='bg-slate-600 p-2 rounded-md m-2'
+			className='bg-slate-600 hover:bg-slate-400 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear'
 			onClick={() => signIn()}
 		>
 			SignIn
@@ -15,7 +15,7 @@ export const SignInButton = () => {
 export const SignOutButton = () => {
 	return (
 		<button
-			className='bg-slate-600 p-2 rounded-md m-2'
+			className='bg-slate-600 hover:bg-slate-400 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear'
 			onClick={() => signOut()}
 		>
 			SignOut
